68207cbcd78375f147f508ec30fe20bd45845fd3
Hugo Culot <hugoculot.dev@gmail.com>
2026-04-21 20:00:08 +0200

    Added hover to card (but has i bug does not come back to original position)

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public CardScriptableObjectScript cardObject;
+    SpriteRenderer spriteRenderer;
+    Vector3 initialPos;
+    bool selected = false;
+    [HideInInspector] public int handPosId;
+    public int boardPosId;
+
+    private void Awake() {
+        cardObject = Resources.Load(gameObject.name.Replace("(Clone)", "")) as CardScriptableObjectScript;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnMouseEnter() {
+        if (initialPos == Vector3.zero)
+        {
+            initialPos = transform.position;
+        }
+        if (selected) return;
+
+        spriteRenderer.sortingOrder = 5;
+
+        // Déplace la carte vers le haut
+        iTween.MoveTo(gameObject, initialPos + transform.up / 20, .1f);
+    }
+
+    private void onMouseExit() {
+        if (selected) return;
+
+        spriteRenderer.sortingOrder = 1;
+        iTween.MoveTo(gameObject, initialPos, .1f);
+    }
+
+    private void OnMouseUp() {
+        // clic
     }
 }
