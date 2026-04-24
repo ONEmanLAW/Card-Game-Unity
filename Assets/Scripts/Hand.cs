5653756a2ba1f1830af214a6856f45bc57c64cc3
Hugo Culot <hugoculot.dev@gmail.com>
2026-04-24 19:28:40 +0200

    Instantiate hand cards at card positions

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -9,22 +9,34 @@ public class Hand : MonoBehaviour
     private static Hand instance = null;
     public static Hand Instance => instance;
 
-    private void Awake() {
-        if (instance != null && instance != this) {
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
             Destroy(this.gameObject);
             return;
-        } else {
+        }
+        else
+        {
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
     }
 
-    public void InitializeHand(GameObject[] cards) {
+    public void InitializeHand(GameObject[] cards)
+    {
         handCards = cards;
 
-        for (int i = 0; i < 4; i++) {
-            GameObject card = Instantiate(handCards[i]);
-            card.GetComponent<Card>().handPosId = i;
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject card = Instantiate(handCards[i], cardPos[i].position, cardPos[i].rotation);
+            Card cardComponent = card.GetComponent<Card>();
+
+            if (cardComponent != null)
+            {
+                cardComponent.handPosId = i;
+            }
+
             SetCardPosInHand(i, card);
         }
     }
@@ -33,19 +45,27 @@ public class Hand : MonoBehaviour
     {
         handCards[index] = Deck.Instance.GetCardFromDeck();
 
-        GameObject card = Instantiate(handCards[index]);
-        
+        GameObject card = Instantiate(handCards[index], cardPos[index].position, cardPos[index].rotation);
+        Card cardComponent = card.GetComponent<Card>();
+
+        if (cardComponent != null)
+        {
+            cardComponent.handPosId = index;
+        }
+
         SetCardPosInHand(index, card);
         Invoke("GoBackToHeadView", 1f);
     }
 
-    public void GoBackToHeadView() {
+    public void GoBackToHeadView()
+    {
         CamController.Instance.GoToHeadView();
     }
 
     private void SetCardPosInHand(int index, GameObject card)
     {
-        iTween.MoveTo(card, cardPos[index].position, .5f);
-        iTween.RotateTo(card, cardPos[index].rotation.eulerAngles, .5f);
+        iTween.Stop(card);
+        card.transform.position = cardPos[index].position;
+        card.transform.rotation = cardPos[index].rotation;
     }
 }
