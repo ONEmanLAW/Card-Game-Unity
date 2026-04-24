5c5e972fa76bf77b7d999ae5c4272ddb84fa0608
Hugo Culot <hugoculot.dev@gmail.com>
2026-04-24 18:42:48 +0200

    Renamed folder for working cards

--- Assets/Scripts/CardScriptableObjectScript.cs
+++ Assets/Scripts/CardScriptableObjectScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CardScriptableObjectScript", menuName = "Scriptable Objects/CardScriptableObjectScript")]
 public class CardScriptableObjectScript : ScriptableObject
 {
-    public string name;
+    public string cardName;
     public int cost;
     public int atk;
     public int def;
